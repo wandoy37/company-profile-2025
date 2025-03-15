0341e6a699504e7b16b520de4120e188bc9fddb5
wandoy37 <riswandi035@gmail.com>
2025-03-15 17:43:33 +0800

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Login</title>
+
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets') }}/favicon.png">
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('assets') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('assets') }}/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-info">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-6">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <img src="{{ asset('assets') }}/favicon.png" class="img-fluid my-4"
+                                            width="25%" alt="">
+                                        <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                    </div>
+
+                                    @if ($errors->any())
+                                        <div class="alert alert-warning" role="alert">
+                                            Login gagal, email / password anda salah !
+                                        </div>
+                                    @endif
+
+                                    <form action="{{ route('login') }}" class="user" method="POST">
+                                        @csrf
+                                        <div class="form-group">
+                                            <input type="email" name="email"
+                                                class="form-control form-control-user @error('email') is-invalid @enderror"
+                                                placeholder="Enter Email Address...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="password"
+                                                class="form-control form-control-user" placeholder="Password">
+                                        </div>
+                                        <button type="submit" class="btn btn-info btn-user btn-block">Login</button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('assets') }}/vendor/jquery/jquery.min.js"></script>
+    <script src="{{ asset('assets') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('assets') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('assets') }}/js/sb-admin-2.min.js"></script>
+
+    <script>
+        window.setTimeout(function() {
+            //$(".custom-alert").alert('close'); <--- Do not use this
+
+            $(".alert").slideUp(500, function() {
+                $(this).remove();
+            });
+        }, 3000);
+    </script>
+
+</body>
+
+</html>
